bdda55d3504c6d6f2c0cccf7befde8b88580c256
Justin-mario <79143260+Justin-mario@users.noreply.github.com>
2023-05-05 14:11:55 +0100

    second commit

--- app/src/main/java/com/example/android/artspaceapp/MainActivity.kt
+++ app/src/main/java/com/example/android/artspaceapp/MainActivity.kt
@@ -1,16 +1,28 @@
 package com.example.android.artspaceapp
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.StringRes
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.android.artspaceapp.ui.theme.ArtSpaceAppTheme
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,25 +34,48 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ArtSpaceAppPreview()
                 }
             }
         }
     }
 }
 
+
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ButtonElement(@StringRes text: Int, modifier: Modifier = Modifier){
+    Text(text = stringResource(id = text))
+
+}
+
+@Composable
+fun ArtSpaceApp(modifier: Modifier = Modifier) {
+    Column() {
+//        Image(painter = painterResource(id = R.drawable.ic_launcher_background), contentDescription = null)
+
+        Column() {
+            Text(text = "")
+            Text(text = "")
+        }
+
+        Row(modifier = modifier.fillMaxWidth()) {
+            ButtonElement(text = R.string.previous,
+                modifier = modifier
+                    .wrapContentWidth(Alignment.Start))
+            ButtonElement(text = R.string.next,
+                modifier = modifier
+                    .wrapContentWidth(Alignment.End))
+        }
+
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ArtSpaceAppPreview() {
     ArtSpaceAppTheme {
-        Greeting("Android")
+        ArtSpaceApp()
     }
 }
