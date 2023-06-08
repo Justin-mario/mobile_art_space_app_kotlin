771bb2f1817825c864c1cbf4375820f3977507cd
Justin-mario <79143260+Justin-mario@users.noreply.github.com>
2023-06-08 23:39:21 +0100

    second commit

--- app/src/main/java/com/example/android/artspaceapp/MainActivity.kt
+++ app/src/main/java/com/example/android/artspaceapp/MainActivity.kt
@@ -1,28 +1,46 @@
 package com.example.android.artspaceapp
 
-import android.media.Image
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.StringRes
-
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ButtonDefaults.shape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.android.artspaceapp.ui.theme.ArtSpaceAppTheme
-import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,7 +52,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ArtSpaceAppPreview()
+                    ArtSpaceApp()
                 }
             }
         }
@@ -45,32 +63,73 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ButtonElement(@StringRes text: Int, modifier: Modifier = Modifier){
-    Text(text = stringResource(id = text))
+    Button(onClick = { /*TODO*/ }) {
+        Text(text = stringResource(id = text))
+
+
+    }
 
 }
 
 @Composable
 fun ArtSpaceApp(modifier: Modifier = Modifier) {
-    Column() {
-//        Image(painter = painterResource(id = R.drawable.ic_launcher_background), contentDescription = null)
 
-        Column() {
-            Text(text = "")
-            Text(text = "")
+        Card(modifier = modifier
+            .fillMaxWidth()
+            .padding(12.dp)
+            .background(color = Color.White),
+            shape = RoundedCornerShape(4.dp),
+            elevation = CardDefaults.cardElevation(4.dp)
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally) {
+                Text(text = "IFE ART")
+                Image(painter = painterResource(id = R.drawable.ife_art),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    contentScale = ContentScale.Crop,)
+
+                Text(text = "This is an art from ile-ife")
+
+
+                    Row(modifier = Modifier
+                        .padding(16.dp)
+                        .fillMaxWidth()
+                    ) {
+                        Box(modifier = Modifier.weight(0.7f)) {
+                            ButtonElement(
+                                text = R.string.previous,
+                                modifier = Modifier
+                            )
+                        }
+                    Spacer(modifier = Modifier.weight(0.9f))
+                        Box(modifier = Modifier
+
+                            .weight(0.5f)) {
+                            ButtonElement(text = R.string.next,
+                                modifier = modifier,
+
+
+
+
+                            )
+                        }
+
+                    }
+
+
+            }
+
+            }
         }
 
-        Row(modifier = modifier.fillMaxWidth()) {
-            ButtonElement(text = R.string.previous,
-                modifier = modifier
-                    .wrapContentWidth(Alignment.Start))
-            ButtonElement(text = R.string.next,
-                modifier = modifier
-                    .wrapContentWidth(Alignment.End))
-        }
 
-    }
 
-}
+
+
 
 @Preview(showBackground = true)
 @Composable
